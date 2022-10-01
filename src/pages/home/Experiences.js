871e55c229ad40811b1e0fe751985d34607cd4dd
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-10-01 14:02:37 +0530

    experiences

--- src/pages/home/Experiences.js
+++ src/pages/home/Experiences.js
@@ -1,27 +1,48 @@
 import React, { useState } from "react";
 import SectionTitle from "../../components/SectionTitle";
-import {experiences} from "../../resources/experiences"
+import { experiences } from "../../resources/experiences";
 
 function Experiences() {
   const [selectedItemIndex, setSelectedItemIndex] = useState(0);
-  return <div>
-    <SectionTitle title={"Experience"} />
-    <div className="flex py-10 gap-10">
-      <div className="flex flex-col gap-10 border-l-2 border-[#135e4c82]">
-        {experiences.map((experience, index) => (
-          <div onClick={() => setSelectedItemIndex(index)} className="cursor-pointer">
-            <h1 className={`text-xl px-5 ${selectedItemIndex === index ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a31] py-3" : "text-white"}`}>{experience.period}</h1>
-          </div>
-        ))}
-      </div>
-      <div className="flex flex-col gap-5"> 
-        <h1 className="text-secondary text-xl">{experiences[selectedItemIndex].title}</h1>
-        <h1 className="text-tertiary text-xl">{experiences[selectedItemIndex].company}</h1>
-        <p className="text-white text-xl">{experiences[selectedItemIndex].description}</p>
+  return (
+    <div>
+      <SectionTitle title={"Experience"} />
+      <div className="w-full flex py-10 sm:py-5 gap-[200px] sm:gap-10 sm:flex-col">
+        <div className=" flex flex-col gap-10 border-l-2 border-[#135e4c82] sm:flex-row sm:overflow-x-scroll sm:w-full sm:border-l-0 sm:justify-center">
+          {experiences.map((experience, index) => (
+            <div
+              onClick={() => {
+                setSelectedItemIndex(index);
+              }}
+              className="cursor-pointer"
+            >
+              <h1
+                className={`text-xl px-5 sm:text-sm
+                 ${
+                   selectedItemIndex === index
+                     ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-[#1a7f5a31] py-3 sm:ml-0"
+                     : "text-white"
+                 } `}
+              >
+                {experience.period}
+              </h1>
+            </div>
+          ))}
+        </div>
+        <div className="w-[50vw] sm:w-full flex flex-col gap-5">
+          <h1 className="text-secondary text-xl">
+            {experiences[selectedItemIndex].title}
+          </h1>
+          <h1 className="text-tertiary text-xl">
+            {experiences[selectedItemIndex].company}
+          </h1>
+          <p className="text-white text-xl sm:text-sm text-justify">
+            {experiences[selectedItemIndex].description}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-
+  );
 }
 
 export default Experiences;
--- src/resources/experiences.js
+++ src/resources/experiences.js
@@ -2,21 +2,21 @@ export const experiences = [
   {
     company: "CDIX Innovation Pvt. Ltd.",
     title: "Full Stack Developer",
-    period: "2022-Present",
+    period: "2022",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     company: "Youtube",
     title: "Web Developemet Instructor",
-    period: "2022-Present",
+    period: "2022",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     company: "Self-Employed",
     title: "FreeLancer Achitect",
-    period: "2020-2022",
+    period: "2020",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
