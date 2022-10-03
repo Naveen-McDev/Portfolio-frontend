7b86d35a500801f8c44899663a4a2c14e767fd9c
Naveen Krish <98112231+Naveen-McDev@users.noreply.github.com>
2022-10-03 19:39:17 +0530

    experiences add, edit and delete functionality

--- src/pages/admin/AdminExperiences.js
+++ src/pages/admin/AdminExperiences.js
@@ -1,13 +1,85 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import { Form, message, Modal } from "antd";
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { HideLoading, ReloadData, ShowLoading } from "../../redux/rootSlice";
 
 function AdminExperiences() {
+  const dispatch = useDispatch();
   const { portfolioData } = useSelector((state) => state.root);
   const { experiences } = portfolioData;
+  const [showAddEditModal, setShowAddEditModal] = useState(false);
+  const [selectedItemForEdit, setSelectedItemForEdit] = useState(null);
+  const [type, setType] = useState("add");
+
+  const onFinish = async (values) => {
+    try {
+      dispatch(ShowLoading());
+      let response;
+      if (selectedItemForEdit) {
+        response = await axios.post(
+          `${process.env.REACT_APP_SERVER_URL}/api/portfolio/update-experience`,
+          {
+            ...values,
+            _id: selectedItemForEdit._id,
+          }
+        );
+      } else {
+        response = await axios.post(
+          `${process.env.REACT_APP_SERVER_URL}/api/portfolio/add-experience`,
+          values
+        );
+      }
+
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+        setShowAddEditModal(false);
+        setSelectedItemForEdit(null);
+        dispatch(HideLoading());
+        dispatch(ReloadData(true));
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  const onDelete = async (item) => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/api/portfolio/delete-experience`,
+        {
+          _id: item._id,
+        }
+      );
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+        dispatch(HideLoading());
+        dispatch(ReloadData(true));
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
   return (
     <div>
       <div className="flex justify-end">
-        <button className="bg-primary px-5 py-2 text-white">
+        <button
+          className="bg-primary px-5 py-2 text-white"
+          onClick={() => {
+            setSelectedItemForEdit(null);
+            setShowAddEditModal(true);
+          }}
+        >
           Add Experience
         </button>
       </div>
@@ -23,14 +95,74 @@ function AdminExperiences() {
             <h1>Role : {experience.title}</h1>
             <h1>{experience.description}</h1>
             <div className="flex justify-end gap-5 mt-5">
-              <button className="bg-red-500 text-white px-5 py-2">
+              <button
+                className="bg-red-500 text-white px-5 py-2"
+                onClick={() => {
+                  onDelete(experience);
+                }}
+              >
                 Delete
               </button>
-              <button className="bg-primary text-white px-5 py-2">Edit</button>
+              <button
+                className="bg-primary text-white px-5 py-2"
+                onClick={() => {
+                  setSelectedItemForEdit(experience);
+                  setShowAddEditModal(true);
+                  setType("edit");
+                }}
+              >
+                Edit
+              </button>
             </div>
           </div>
         ))}
       </div>
+
+      {(type === "add" || selectedItemForEdit) && (
+        <Modal
+          visible={showAddEditModal}
+          title={selectedItemForEdit ? "Edit Experience" : "Add Experience"}
+          footer={null}
+          onCancel={() => {
+            setShowAddEditModal(false);
+            setSelectedItemForEdit(null);
+          }}
+        >
+          <Form
+            layout="vertical"
+            onFinish={onFinish}
+            initialValues={selectedItemForEdit || {}}
+          >
+            <Form.Item name="period" label="Period">
+              <input placeholder="Period" />
+            </Form.Item>
+            <Form.Item name="company" label="Company">
+              <input placeholder="Company" />
+            </Form.Item>
+            <Form.Item name="title" label="Title">
+              <input placeholder="Title" />
+            </Form.Item>
+            <Form.Item name="description" label="Description">
+              <input placeholder="Description" />
+            </Form.Item>
+
+            <div className="flex justify-end">
+              <button
+                className="border-primary text-primary px-5 py-2"
+                onClick={() => {
+                  setShowAddEditModal(false);
+                  setSelectedItemForEdit(null);
+                }}
+              >
+                Cancel
+              </button>
+              <button className="bg-primary text-white px-5 py-2">
+                {selectedItemForEdit ? "Update" : "Add"}
+              </button>
+            </div>
+          </Form>
+        </Modal>
+      )}
     </div>
   );
 }
